09550a2175522279f8f5148d29a91b67fbe82c1d
Raky-S <rakysy1@gmail.com>
2020-11-23 00:16:36 +0100

    amazonClone seconde commit

--- amazonClone/src/Header.js
+++ amazonClone/src/Header.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+import { ShoppingBasket } from "@material-ui/icons";
+
 
 function Header() {
   return (
@@ -10,17 +13,28 @@ function Header() {
       />
       <div className="header_search">
         <input className="header_searchInput" type=" text" />
-        {/*Logo*/}
+        <SearchIcon
+          className="header_searchIcon" />
       </div>
       <div className="header_nav">
         <div className="header_option">
-          <span className="header_optionOne">Hello Guest</span>
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
         </div>
+
         <div className="header_option">
-          <span className="header_optionTwo">Sign In</span>
+          <span className="header_optionLineOne">Returns</span>
+          <span className="header_optionLineTwo">& Orders</span>
         </div>
+
         <div className="header_option">
-          <span className="header_optionThree">Prime</span>
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header_optionBasket">
+          <ShoppingBasket />
+          <span className="header_optionLineTwo header_basketCount">0</span>
         </div>
       </div>
     </div>
