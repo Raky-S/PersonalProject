bee24285ce5e46973d4ab460039b28aeb105f959
Raky-S <68588039+Raky-S@users.noreply.github.com>
2020-12-06 22:51:27 +0100

    Delete axios.js

--- amazon-full-stack-clone/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    // THE API (cloud function) URL
-    baseURL: 'https://us-central1-clone-7e2a7.cloudfunctions.net/api'
-    // 'http://localhost:5001/clone-7e2a7/us-central1/api'
-})
-
-export default instance;
